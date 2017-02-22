3712a50821fafe1afba20a0e058c65e212613207
Luis Dalmolin <luis.nh@gmail.com>
2017-02-22 15:15:09 -0300

    Changed composer

--- src/Historiable.php
+++ src/Historiable.php
@@ -1,9 +1,23 @@
-<?php 
+<?php
 
 namespace Dalmolin\Historiable;
 
 trait Historiable
 {
 
-	
+    /**
+     * Boot the medias trait for a model.
+     *
+     * @return void
+     */
+    public static function bootMedias()
+    {
+        static::saving(function($model) {
+            // do it
+        });
+
+        static::deleting(function($model) {
+            // do it
+        });
+    }
 }
